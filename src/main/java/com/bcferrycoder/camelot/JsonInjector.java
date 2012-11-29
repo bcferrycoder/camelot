a701d7c8c1cc8e0d79fa95de61a32bbc3c3b7622
John Wetherill <johnw@activestate.com>
2012-11-29 01:30:53 -0800

    updates in prep for delivery

---
+++ src/main/java/com/bcferrycoder/camelot/JsonInjector.java
@@ -0,0 +1,19 @@
+package com.bcferrycoder.camelot;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stackato
+ * Date: 11/22/12
+ * Time: 2:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JsonInjector implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        exchange.getIn().setBody("{\"movielist\":\"raiders\"}");
+        exchange.getOut().setBody("{\"movielist\":\"raiders\"}");
+    }
+}
